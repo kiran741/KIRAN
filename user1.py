2799d959dc5f3bf6f6181d03596686c79607b916
kiran741 <31614742+kiran741@users.noreply.github.com>
2017-09-04 14:09:58 +0530

    Add files via upload

---
+++ user1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+a=23
+print(a*4)
+print(a/3)
+print(a+3)
+print(a-5)
+print(a*55)
+print("cmr is a big university")
+print("cmr has a very big campus")
+print("i study in cmr university")
+print("there is a lab in the college")
+print("cmr*6")
+print(646*65654)
---
+++ user2.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+import random
+r=random.randint(1,100)
+print(r)
