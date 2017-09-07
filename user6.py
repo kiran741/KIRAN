5d0325ee67ef5279a5230e0831810661123cd8ad
kiran741 <31614742+kiran741@users.noreply.github.com>
2017-09-07 10:33:16 +0530

    Add files via upload

---
+++ user6.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import smtplib
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+s.login("kiranrocks5005@gmail.com", "STSAPOKIVA") 
+message = "hi"
+s.sendmail("kiranrocks5005@gmail.com", "kiranrocks5005@gmail.com",message)
+s.quit()
