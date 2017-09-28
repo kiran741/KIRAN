c5ffde11adb7405f57e635894abbf53d072a02c7
kiran741 <31614742+kiran741@users.noreply.github.com>
2017-09-28 09:05:34 +0530

    Update user6.py

--- user6.py
+++ user6.py
@@ -3,6 +3,7 @@
 import smtplib
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
+#Loggging in to ur account
 s.login("kiranrocks5005@gmail.com", "STSAPOKIVA") 
 message = "hi"
 s.sendmail("kiranrocks5005@gmail.com", "kiranrocks5005@gmail.com",message)
